53d3178cbd685c5d307c22e67044e2e4891cb90a
EM4Volts <87427011+EM4Volts@users.noreply.github.com>
2022-01-05 16:25:29 +0100

    Add files via upload

---
+++ Firmware/5plank.c
@@ -0,0 +1,4 @@
+// Copyright 2022 EM4Volts (@EM4Volts)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include "5plank.h"
---
+++ Firmware/keymaps/default/keymap.c
@@ -0,0 +1,17 @@
+// Copyright 2022 EM4Volts (@EM4Volts)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include QMK_KEYBOARD_H
+
+// Defines names for use in layer keycodes and the keymap
+enum layer_names {
+    _BASE
+    _FN
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /* Base */
+    [_BASE] = LAYOUT(
+        KC_A,    KC_B,    KC_C,    KC_D,    KC_E,    KC_F
+    )
+};
